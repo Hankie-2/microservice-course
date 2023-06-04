6595893789d686a22a6fa10a2b2f4531d66c934f
Marsel Charginov <marselcharginov2002@gmail.com>
2023-06-04 19:45:16 +0600

    add eureka server and eureka client, implement LoadBalanced

--- eureka-server/src/main/java/kg/charginov/EurekaServerApplication.java
+++ eureka-server/src/main/java/kg/charginov/EurekaServerApplication.java
@@ -2,17 +2,18 @@ package kg.charginov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Hello world!
+ *
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class CustomerApplication
+@EnableEurekaServer
+public class EurekaServerApplication
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(CustomerApplication.class,args);
+        SpringApplication.run(EurekaServerApplication.class,args);
     }
 }
